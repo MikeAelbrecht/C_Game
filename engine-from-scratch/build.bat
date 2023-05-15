88b9a4206091fd3cae47408c3b706bab45cf4adb
Mike Aelbrecht <mikeaelbrecht@gmail.com>
2023-05-16 00:26:01 +0200

    EP3 - Still minor bug

--- engine-from-scratch/build.bat
+++ engine-from-scratch/build.bat
@@ -1,4 +1,6 @@
-set files=src\glad.c src\main.c src\engine\global.c src\engine\render\render.c src\engine\render\render_init.c
+set render=src\engine\render\render.c src\engine\render\render_init.c src\engine\render\render_util.c
+set io=src\engine\io\io.c
+set files=src\glad.c src\main.c src\engine\global.c %render% %io%
 set libs=E:\C_Game\lib\SDL2main.lib E:\C_Game\lib\SDL2.lib E:\C_Game\lib\freetype.lib
 
 CL /Zi /I E:\C_Game\include %files% /link %libs% /OUT:mygame.exe
