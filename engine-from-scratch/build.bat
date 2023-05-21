9bc1731619c465f42efc8a56113eae5857e02802
Mike Aelbrecht <mikeaelbrecht@gmail.com>
2023-05-22 00:49:18 +0200

    EP4 Done - Escape doesn't exit the program

--- engine-from-scratch/build.bat
+++ engine-from-scratch/build.bat
@@ -1,6 +1,9 @@
 set render=src\engine\render\render.c src\engine\render\render_init.c src\engine\render\render_util.c
 set io=src\engine\io\io.c
-set files=src\glad.c src\main.c src\engine\global.c %render% %io%
-set libs=E:\C_Game\lib\SDL2main.lib E:\C_Game\lib\SDL2.lib E:\C_Game\lib\freetype.lib
+set config=src\engine\config\config.c
+set input=src\engine\input\input.c
+set time=src\engine\time\time.c
+set files=src\glad.c src\main.c src\engine\global.c %render% %io% %config% %input% %time%
+set libs=E:\C_Game\lib\SDL2main.lib E:\C_Game\lib\SDL2.lib
 
 CL /Zi /I E:\C_Game\include %files% /link %libs% /OUT:mygame.exe
