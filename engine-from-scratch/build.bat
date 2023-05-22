7447813031a888dcb590cc934f38f7c08b916c68
Mike Aelbrecht <mikeaelbrecht@gmail.com>
2023-05-23 00:15:50 +0200

    EP5 - No Errors/Bugs :)

--- engine-from-scratch/build.bat
+++ engine-from-scratch/build.bat
@@ -3,7 +3,9 @@ set io=src\engine\io\io.c
 set config=src\engine\config\config.c
 set input=src\engine\input\input.c
 set time=src\engine\time\time.c
-set files=src\glad.c src\main.c src\engine\global.c %render% %io% %config% %input% %time%
+set physics=src\engine\physics\physics.c
+set array_list=src\engine\array_list\array_list.c
+set files=src\glad.c src\main.c src\engine\global.c %render% %io% %config% %input% %time% %physics% %array_list%
 set libs=E:\C_Game\lib\SDL2main.lib E:\C_Game\lib\SDL2.lib
 
 CL /Zi /I E:\C_Game\include %files% /link %libs% /OUT:mygame.exe
